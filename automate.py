b5fa57010c65186365861914393d31adae3495bd
VinayakaHk <32355181+VinayakaHk@users.noreply.github.com>
2021-06-03 16:52:08 +0530

    Update automate.py

--- automate.py
+++ automate.py
@@ -4,17 +4,37 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.keys import Keys
+import time
+import sys
+
+class AnyEC:
+    def __init__(self, *args):
+        self.ecs = args
+
+    def __call__(self, driver):
+        for f in self.ecs:
+            try:
+                res = f(driver)
+                if res:
+                    return res
+
+            except:
+                pass
+
+
+def print_time():
+    return str(time.localtime().tm_hour) + ":" + str(time.localtime().tm_min) + ":" + str(time.localtime().tm_sec)
 
 
 def wait_for_conf_click():
     while True:
         try:
-            print("I cant listen")
-            driver.find_element_by_xpath("/html/body/div[2]/div/div")
-            driver.implicitly_wait(60)
+            WebDriverWait(
+                driver, 1).until(
+                ec.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div")))
             continue
-        except NoSuchElementException:
-            print("Ok, now , i can listen")
+        except (NoSuchElementException, TimeoutException):
+            print("Ok, now , i can listen. Time:" + str(print_time()))
             break
 
 
@@ -22,6 +42,7 @@ def wait_for_listen_button():
     while True:
         try:
             driver.find_element_by_xpath("/html/body/div[2]/div/div")
+            print("Located click button, time: " + str(print_time()))
             break
         except NoSuchElementException:
             continue
@@ -31,6 +52,7 @@ def wait_for_homepage():
     while True:
         try:
             driver.find_element_by_xpath("/html/body/div[1]/div/div/header/div/h6/img")
+            print("Homepage located, time = " + str(print_time()))
             break
         except NoSuchElementException:
             continue
@@ -43,14 +65,26 @@ def text_public():
 
 
 def wait_class_end():
-
     while True:
         try:
-            WebDriverWait(
-                driver, 60).until(
+            #  Searching for the button returning break, doesnt work in some cases
+            #  WebDriverWait(
+            #      driver, 60).until(
+            #      ec.presence_of_element_located((
+            #          By.XPATH, "/html/body/div[1]/div/div/div/div/button")))
+            web1 = WebDriverWait(
+                driver, 60).until(AnyEC(
                 ec.presence_of_element_located((
-                    By.XPATH, "/html/body/div/div/div/div/div/button")))
+                        By.XPATH, "/html/body/div/div/div/div")),
+                ec.presence_of_element_located((
+                    By.CLASS_NAME, "message--2dMxKe")),
+                ec.presence_of_element_located((By.CLASS_NAME,"/html/body/div/div/h1[1]"))))
             break
+
+        #  div class(flex) : modal--MalHB
+        #  div class : content--PAEoo
+        #  xpath button: /html/body/div/div/div/div/button
+        #  button class : button--Z2dosza md--Q7ug4 primary--1IbqAO button--Z1j2w3P
         except TimeoutException:
             continue
 
@@ -72,14 +106,23 @@ def join_class():
 
 
 def login_system():
-    driver.find_element_by_name("user_email").send_keys("USN")
-    driver.find_element_by_name("user_password").send_keys("pass")
+    driver.find_element_by_name("user_email").send_keys("username")
+    driver.find_element_by_name("user_password").send_keys("password")
     driver.find_element_by_xpath("//div[@id='root']/div/div/div[2]/div/div/div/div/form/div[3]/button/span").click()
     # submit button because there is no input tag with unique ID
+    print("Time now: " + str(print_time()))
 
 
+def main():
+    try:
+        init()
+
+    except KeyboardInterrupt:
+        print("Error: Program execution interrupted.", file=sys.stderr)
+
+print("Program started at : " + str(print_time()))
 driver = webdriver.Firefox(executable_path="geckodriver.exe")
-driver.maximize_window()
+driver.fullscreen_window()
 driver.get("https://alive.university/")
 login_system()
 wait_for_homepage()
@@ -88,22 +131,25 @@ while True:
     join_class()
     # print("Waiting 20 seconds for it to load up")
     # instead of waiting for x amount of time, pick an element and  the class to load
-    print("iframe  retrieval")
+    print("iframe  retrieval" + str(print_time()))
     driver.switch_to.frame("embedd-bbb")
-    print("clicking on listen")
+    print("Switched to frame, clicking on listen" + str(print_time()))
     wait_for_listen_button()
     driver.find_element_by_xpath("//button[2]/span/i").click()
     wait_for_conf_click()
     # to type anything in the public chat and send it
     # text_public()
-    print("click on hide chat button")
+    print("click on hide chat button" + str(print_time()))
     driver.find_element_by_xpath("/html/body/div/main/section/div[1]/header/div/div[1]/div[1]/button").click()
-    print("waits for the class to end by using wait_class_end()")
+    print("waits for the class to end by using wait_class_end(). Time:" + str(print_time()))
     wait_class_end()
-    print(" after class ends")
+    print(" after class ends, Time: " + str(print_time()))
     driver.switch_to.parent_frame()
-    print("clicking on home after class ends.")
+    print("clicking on home after class ends. Time: " + str(print_time()))
     driver.find_element_by_xpath("//div[@id='root']/div/div/header/div/div[2]/button/span").click()
-    print('Phew! Class ended')
-
+    print('Phew! Class ended. Time: ' + str(print_time()))
 # driver.close()
+
+
+if __name__ == "__main__":
+    main()
