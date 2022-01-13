37eff7e5defcdb2c5283efa2b2591d04d27fb647
VinayakaHk <32355181+VinayakaHk@users.noreply.github.com>
2022-01-13 12:19:05 +0530

    v2022.v1

--- automate.py
+++ automate.py
@@ -8,7 +8,6 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.keys import Keys
 import time
 import sys
-from time import sleep
 
 
 class AnyEC:
@@ -47,10 +46,17 @@ def wait_for_listen_button():
     print('wait_for_listen_button. Time: ' + str(print_time()))
     while True:
         try:
-            driver.find_element_by_xpath("/html/body/div[2]/div/div")
+            WebDriverWait(
+                driver,60).until(
+                ec.presence_of_element_located((By.CSS_SELECTOR, "button.jumbo--Z12Rgj4:nth-child(2)")))
             print("Located Listen button. time: " + str(print_time()))
+        except (NoSuchElementException,TimeoutException):
+            continue
+        try:
+            driver.find_element_by_css_selector("button.jumbo--Z12Rgj4:nth-child(2)").click()
+            print("Clicked on Listen button. time: " + str(print_time()))
             break
-        except NoSuchElementException:
+        except (NoSuchElementException,TimeoutException):
             continue
 
 
@@ -64,7 +70,6 @@ def wait_for_homepage():
         except NoSuchElementException:
             continue
 
-
 def text_public():
     print('Text Public. Time: ' + str(print_time()))
     public_chat = driver.find_element_by_xpath('//*[@id="message-input"]')
@@ -76,20 +81,12 @@ def wait_class_end():
     print('wait_class_end. Time: ' + str(print_time()))
     while True:
         try:
-
+ 
             WebDriverWait(
                 driver, 60).until(
-                ec.presence_of_element_located((
-                    By.XPATH, "/html/body/div/div/div/div/div/button/span")))
+                 ec.presence_of_element_located((
+                     By.XPATH, "/html/body/div/div/div/div/div/button")))
             print("Class ended " + str(print_time()))
-
-# Before Editing =  /html/body/div/div/div/div
-# css- selector = .label--Z12LMR3
-# css- path = html.animationsEnabled body.browser-firefox.os-windows div#app div.parent--ZQqoCg div.modal--MalHB div.content--PAEoo div button.button--Z2dosza.md--Q7ug4.primary--1IbqAO.button--Z1j2w3P span.label--Z12LMR3
-# xpath = /html/body/div/div/div/div/div/button/span
-# Inner HTML = <span class="label--Z12LMR3">OK</span>
-# Outer html = <button aria-label="OK" aria-disabled="false" class="button--Z2dosza md--Q7ug4 primary--1IbqAO button--Z1j2w3P" description="Logs you out of the meeting"><span class="label--Z12LMR3">OK</span></button>
-
             break
         except TimeoutException:
             continue
@@ -136,8 +133,8 @@ def hide_chat():
 
 def login_system():
     print("Logging in login_system(). Time: " + str(print_time()))
-    driver.find_element_by_name("user_email").send_keys("USN")
-    driver.find_element_by_name("user_password").send_keys("PASS")
+    driver.find_element_by_name("user_email").send_keys("username")
+    driver.find_element_by_name("user_password").send_keys("password")
     submit_btn = driver.find_element_by_css_selector(".MuiButtonBase-root")
     submit_btn.click()
     print("Logged in. Time: " + str(print_time()))
@@ -161,7 +158,6 @@ def main():
         print("Terminating program.")
         driver.close()
         driver.quit()
-        sleep(60)
 
 
 def init():
@@ -173,41 +169,23 @@ def init():
     driver.get("https://alive.university")
     print("Site opened.")
     login_system()
+    
     wait_for_homepage()
 
     while True:
         join_class()
-        # print("Waiting 20 seconds for it to load up")
-        # instead of waiting for x amount of time, pick an element and  the class to load
-        sleep(10)
         print("iframe  retrieval : " + str(print_time()))
         driver.switch_to.frame("embedd-bbb")
         print("Switched to frame, clicking on listen , time : " + str(print_time()))
+        
         wait_for_listen_button()
-        try:
-            driver.find_element_by_xpath("//button[2]/span/i").click()
-        except WebDriverException as err:
-            print ("Listen Button (//button[2]/span/i) not found. Skipping it.")
-        #wait_for_conf_click()
-        # to type anything in the public chat and send it
-        # text_public()
-        print("click on hide chat button, Time :" + str(print_time()))
-        # Old xpath
-
-        #hide_chat()
-
-
-# Edited /html/body/div[1]/main/section/div[4]/header/div/div[1]/div[1]/button/span[1]/i
-# before /html/body/div/main/section/div[1]/header/div/div[1]/div[1]/button
         print("waits for the class to end by using wait_class_end(). Time:" + str(print_time()))
         wait_class_end()
         print("Class has ended, Time: " + str(print_time()))
         driver.switch_to.parent_frame()
-# The below method doesnt work anymore. Have to redirect it directly.
-#        print("clicking on home after class ends. Time: " + str(print_time()))
-#        driver.find_element_by_xpath("/html/body/div[1]/div/div/header/div/div[2]/button[1]/span[1]").click()
         driver.get("https://alive.university/app/main")
         print('Phew! Class ended. Time: ' + str(print_time()))
+        print ('\n__________________________________________________________\n')
 
 
 if __name__ == "__main__":
